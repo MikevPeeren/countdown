79ef6c5964a4f961e06ad8b5c27c9a42d78f1752
MikevPeeren <MikevPeeren@hotmail.com>
2020-06-05 09:52:25 +0200

    Cleanup on Isle 6

---
+++ pages/index.tsx
@@ -0,0 +1,33 @@
+// Next
+import Head from 'next/head';
+
+// CSS
+import './styles.global.scss';
+import styles from './index.scss';
+
+const Home =()=> {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+
+      <footer>
+        {/* <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+        </a> */}
+      </footer>
+    </div>
+  )
+}
+export default Home;
