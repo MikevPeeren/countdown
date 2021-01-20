29c15a44e8d71d3b9d7a797357db85a16a353860
MikevPeeren <MikevPeeren@hotmail.com>
2021-01-20 10:30:06 +0100

    :recycle: updated date

--- pages/components/Countdown.tsx
+++ pages/components/Countdown.tsx
@@ -8,7 +8,7 @@ import styles from './Countdown.scss';
 import { DAYS, HOURS, MINUTES, SECONDS } from '../../constants/general';
 
 const Countdown = () => {
-  const [retirementDate, setRetirementDate] = useState(new Date('Sep 30, 2021 00:00:00').getTime());
+  const [retirementDate, setRetirementDate] = useState(new Date(2021, 7, 27).getTime());
   const [daysLeft, setDaysLeft] = useState(0);
   const [hoursLeft, setHoursLeft] = useState(0);
   const [minutesLeft, setMinutesLeft] = useState(0);
@@ -18,8 +18,10 @@ const Countdown = () => {
     const interval = setInterval(() => {
       // Get today's date
       const today = new Date().getTime();
+
       // Get the difference
       const diff = retirementDate - today;
+
       // Calculate
       setDaysLeft(Math.floor(diff / (1000 * 60 * 60 * 24)));
       setHoursLeft(Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
